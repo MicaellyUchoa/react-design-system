79b18e94fe07252dd263b12129426bab7eaf3d1a
MicaellyUchoa <micaellyit@hotmail.com>
2022-10-13 21:49:12 -0300

    feat: created text component with radix-ui

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from './Text'
+
+export default {
+    title: 'Components/Text',
+    component: Text,
+    args: {
+        children: 'Hello World',
+        size: 'lg'
+    }
+} as Meta<TextProps>
+
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+    args: { size: 'sm' }
+}
+
+export const Medium: StoryObj<TextProps> = {
+    args: { size: 'md' }
+}
+
+export const Large: StoryObj<TextProps> = {
+    args: { size: 'lg' }
+}
+
+export const CustomComponent: StoryObj<TextProps> = {
+    args: { asChild: true, children: (<p>Hello World</p>) }
+}
